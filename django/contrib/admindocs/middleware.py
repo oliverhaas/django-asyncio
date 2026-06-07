320d651a30a2e62f704d93af7d814a8eb637d1a5
Oliver Haas <ohaas@e1plus.de>
2026-06-07 12:08:07 +0200

    Modernize admindocs XViewMiddleware: drop MiddlewareMixin, native async.
    Last stray MiddlewareMixin user among the shipped middleware. process_view
    still works as a plain class method; __call__ is a pass-through.

--- django/contrib/admindocs/middleware.py
+++ django/contrib/admindocs/middleware.py
@@ -1,15 +1,36 @@
+from asgiref.sync import iscoroutinefunction, markcoroutinefunction
+
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpResponse
-from django.utils.deprecation import MiddlewareMixin
 
 from .utils import get_view_name
 
 
-class XViewMiddleware(MiddlewareMixin):
+class XViewMiddleware:
     """
     Add an X-View header to internal HEAD requests.
     """
+
+    sync_capable = True
+    async_capable = True
+
+    def __init__(self, get_response):
+        if get_response is None:
+            raise ValueError("get_response must be provided.")
+        self.get_response = get_response
+        self.is_async = iscoroutinefunction(get_response)
+        if self.is_async:
+            markcoroutinefunction(self)
+        super().__init__()
+
+    def __call__(self, request):
+        if self.is_async:
+            return self.__acall__(request)
+        return self.get_response(request)
+
+    async def __acall__(self, request):
+        return await self.get_response(request)
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         """
